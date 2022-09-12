58840edc5a28821d22787701e89f7d051bbd9d36
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-09-12 22:38:42 +0200

    Create art.py

---
+++ art.py
@@ -0,0 +1,27 @@
+logo = """                                                                                                 
+                                 I8                                                                     
+                                 I8                                                                     
+  gg                          88888888                                                                  
+  ""                             I8                                                                     
+  gg    ,ggg,,ggg,     ,g,       I8      ,gggg,gg    ,gggg,gg   ,gggggg,    ,gggg,gg   ,ggg,,ggg,,ggg,  
+  88   ,8" "8P" "8,   ,8'8,      I8     dP"  "Y8I   dP"  "Y8I   dP"''8I   dP"  "Y8I  ,8" "8P" "8P" !0,
+  88   I8   8I   8I  ,8'  Yb    ,I8,   i8'    '8I  i8'    ,8I  ,8'    8I  i8'    ,8I  I8   8I   8I   8I 
+_,88,_,dP   8I   Yb,,8'_   8)  ,d88b, ,d8,   ,d8b,,d8,   ,d8I ,dP     Y8,,d8,   ,d8b,,dP   8I   8I   Yb,
+8P""Y88P'   8I   `Y8P' "YY8P8P88P""Y88P"Y8888P"`Y8P"Y8888P"8888P      `Y8P"Y8888P"`Y88P'   8I   8I   `Y8
+                                                         ,d8I'                                          
+                                                       ,dP'8I                                           
+                                                      ,8"  8I                                           
+                                                      I8   8I                                           
+                                                      `8, ,8I                                           
+                                                       `Y8Pl                                            
+
+"""
+
+vs = """
+ __      _______ 
+ \ \    / / ____|
+  \ \  / / (___  
+   \ \/ / \___ \ 
+    \  /  ____) |
+     \/  |_____/ 
+"""
