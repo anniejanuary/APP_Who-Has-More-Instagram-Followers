a28219395fa06f3ddd2d2b972114efc0e15a25fb
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-09-13 12:04:00 +0200

    Update APP_Who-Has-More-Instagram-Followers.py

--- APP_Who-Has-More-Instagram-Followers.py
+++ APP_Who-Has-More-Instagram-Followers.py
@@ -1,10 +1,9 @@
-from art import logo
-from art import vs
+from art import logo, vs
 from replit import clear
 import random
 from game_data import data
 
-# Creates "list_of_dicts" with indices of dicts from game_data.py to remove used Insta profiles from the pool
+# Creates "list_of_dicts" with indices of dicts from game_data.py to remove randomly picked Insta profiles from the remaining profile pool
 number_of_dicts = len(data)
 def numbered_dictionaries_list (n1, n2):
     return list(range(n1, n2+1))
@@ -21,20 +20,17 @@ def random_pick(data):
   description = data[random_dict_index]['description']
   country = data[random_dict_index]['country']
   follower_count = int(data[random_dict_index]['follower_count'])
-  return name, description, country, follower_count # function 
+  return name, description, country, follower_count # these are later accessible as tupple indices
 
 
 def display_A_and_B (A, B):
   '''Takes follower numbers from random_pick() functions and asks to compare A vs B'''
   A_followers = A[3]
   B_followers = B[3]
-
   global score
-  
   print(f"A: {A[0]}, a {A[1]} from {A[2]}")
   print(vs)
   print(f"B: {B[0]}, a {B[1]} from {B[2]}")
-
   compare(A_followers, B_followers, A, B)  
 
 
@@ -54,46 +50,28 @@ def compare (A_followers, B_followers, A, B):
   global game_on
 
   if len(list_of_dicts) == 0:
-    print("You've correctly compared all 50 Instagram profiles in my database. You beast, have a damn good day!")
+    print("You've correctly compared all 50 Instagram profiles. You beast!")
     game_on = False
   
   while game_on and len(list_of_dicts) != 0:
     global score
-    user_choice = input("Who has more Instagram followers? Type 'A' or 'B': ").upper() # converting eg. 'a' to 'A'
+    user_choice = input("Who has more Instagram followers? Type 'A' or 'B': ").upper() # eg. 'a' -> 'A'
     if (user_choice == 'A' and A_followers < B_followers) or (user_choice == 'B' and A_followers > B_followers):
       print(f"Sorry, that's incorrect. Final score: {score}")
-
       # play again?
       if input("Would you like to play again? 'y' or 'n': ") == 'y':
         score = 0
         new_game()
       else:
         # end of game
-        game_on = False
-        
-      
+        game_on = False   
     elif (user_choice == 'A' and A_followers > B_followers):
-      
       # loop with same A and new randomized B
-      pick_new_B_and_compare_again (A, B)
-      
+      pick_new_B_and_compare_again (A, B) 
     elif (user_choice == 'B' and A_followers < B_followers):  
-          
-      # B becomes A
-      # clearing A tuple
-      temp_tuple_to_list = list(A)
-      temp_tuple_to_list.clear()
-      # appending B tupple elements to A list
-      temp_tuple_to_list.append(B[0])
-      temp_tuple_to_list.append(B[1])
-      temp_tuple_to_list.append(B[2])
-      temp_tuple_to_list.append(B[3])
-      # turning A list back into a tupple
-      A = tuple(temp_tuple_to_list)
-  
+      A = B    
       # loop with A (old B) and a new randomized B
       pick_new_B_and_compare_again (A, B)
-
     else: 
       print("Enter 'A' or 'B', try again")
       compare (A_followers, B_followers, A, B)
@@ -105,6 +83,11 @@ def new_game():
   global score
   A = random_pick(data)
   B = random_pick(data)
+  # I could write this so that A != B but that wouldnt prevent previously used Insta accounts being
+  # picked from game_data.py:
+  # if A == B:
+  #   B = random_pick(data) -> randomly generate B again
+  
   print(logo)
   display_A_and_B (A, B)
   A_followers = A[3]
@@ -117,5 +100,15 @@ while game_on:
   new_game()
 
 
-
-  
+      ###a super roundabout way to do the same as "A = B" lol:
+      #B becomes A
+      #clearing A tuple
+      # temp_tuple_to_list = list(A)
+      # temp_tuple_to_list.clear()
+      #appending B tupple elements to A list
+      # temp_tuple_to_list.append(B[0])
+      # temp_tuple_to_list.append(B[1])
+      # temp_tuple_to_list.append(B[2])
+      # temp_tuple_to_list.append(B[3])
+      #turning A list back into a tupple
+      # A = tuple(temp_tuple_to_list)
